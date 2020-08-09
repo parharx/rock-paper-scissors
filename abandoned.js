4321434ac4b52eb9f4089c5dd70f84634980727f
parharx <66666579+parharx@users.noreply.github.com>
2020-08-09 18:37:07 +0530

    Add files via upload

---
+++ abandoned.js
@@ -0,0 +1,9 @@
+
+var randomNumber = Math.floor(Math.random() * 3 + 1);
+var randomImage = "img" + randomNumber + ".png";
+var putImage = "image/" + randomImage;
+
+var refresh = document.querySelectorAll("img")[1].setAttribute("src",putImage);
+
+
+    var afterClick === 1;
---
+++ index.js
@@ -0,0 +1,70 @@
+// computer
+
+
+
+$($("button")[0]).click(function (){
+    $($("img")[0]).attr("src","image/img1.png");
+
+    var randomNumber = Math.floor(Math.random() * 3 + 1);
+    var randomImage = "img" + randomNumber + ".png";
+    var putImage = "image/" + randomImage;
+    var refresh = document.querySelectorAll("img")[1].setAttribute("src",putImage);
+
+    if (randomNumber === 2){
+        $("h1").text("You Lost.💔")
+    }
+    else if(randomNumber === 1){
+        $("h1").text("Tie.👀")
+    }
+    else if(randomNumber === 3) {
+        $("h1").text("You win.❤")
+    }
+    else{
+        console.log();
+    }
+
+});
+
+$($("button")[1]).click(function (){
+    $($("img")[0]).attr("src","image/img2.png");
+    var randomNumber = Math.floor(Math.random() * 3 + 1);
+    var randomImage = "img" + randomNumber + ".png";
+    var putImage = "image/" + randomImage;
+    var refresh = document.querySelectorAll("img")[1].setAttribute("src",putImage);
+
+    if (randomNumber === 3){
+        $("h1").text("You Lost.💔")
+    }
+    else if(randomNumber === 2){
+        $("h1").text("Tie.👀")
+    }
+    else if(randomNumber === 1) {
+        $("h1").text("You win.❤")
+    }
+    else{
+        console.log();
+    }
+
+});
+
+$($("button")[2]).click(function (){
+    $($("img")[0]).attr("src","image/img3.png");
+    var randomNumber = Math.floor(Math.random() * 3 + 1);
+    var randomImage = "img" + randomNumber + ".png";
+    var putImage = "image/" + randomImage;
+    var refresh = document.querySelectorAll("img")[1].setAttribute("src",putImage);
+
+    if (randomNumber === 1){
+        $("h1").text("You Lost.💔")
+    }
+    else if(randomNumber === 3){
+        $("h1").text("Tie.👀")
+    }
+    else if(randomNumber === 2) {
+        $("h1").text("You win.❤")
+    }
+    else{
+        console.log();
+    }
+
+});
