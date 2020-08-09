454cea6e71d6cc1a2e9c590b22d6964fdc37ce09
parharx <66666579+parharx@users.noreply.github.com>
2020-08-09 18:43:09 +0530

    Delete abandoned.js

--- abandoned.js
+++
@@ -1,9 +0,0 @@
-
-var randomNumber = Math.floor(Math.random() * 3 + 1);
-var randomImage = "img" + randomNumber + ".png";
-var putImage = "image/" + randomImage;
-
-var refresh = document.querySelectorAll("img")[1].setAttribute("src",putImage);
-
-
-    var afterClick === 1;
